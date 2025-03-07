0362067638d5e4a4cf474ffa3bc9348cbcd6fd3b
Ali <farnudia@iarc.who.int>
2025-03-07 11:40:55 +0100

    Use R from project template generation

--- create_project.R
+++ create_project.R
@@ -1,26 +1,36 @@
-#' Create a New Project Directory and Initialize GitHub Repository
-#'
-#' This function automates the process of creating a project directory, setting up 
-#' necessary subdirectories and  generating a `.gitignore` file. It calls an external 
-#' bash script `create_project.sh`.
-#' 
-#' @param location A character string specifying the base directory where the project 
-#'   should be created.
-#' @param project A character string specifying the project name. This will be used 
-#'   to name the created directory.
-#'
-#' @return This function does not return any value.
-#' 
-#' @examples
-#' # Example of calling create_project with a GitHub username
-#' create_project(location = "/data/MET_share/work/001_projects/", 
-#'                project = "my-new-project")
-#' 
-create_project <- function(location, project, githubname = NULL) {
-  script_path <- "/Users/leem/OneDrive - International Agency for Research on Cancer/001_projects/IARCBiostat/create_project/create_project.sh"
-  cmd <- sprintf(
-    '"%s" --location "%s" --project "%s"',
-    script_path, location, project
-  )
-  system(cmd)
+create_project <- function(location, project, create_project_path = "/data/IARC_Biostat/work/create_project") {
+  if (!dir.exists(location)) {
+    stop(paste("Location directory does not exist:", location))
+  }
+
+  project_path <- file.path(location, project)
+  if (dir.exists(project_path)) {
+    stop(paste0("A project with the name '", project, "' already exists in '", location, "'.\nPlease choose a different name and try again."))
+  } else {
+    dir.create(project_path, recursive = TRUE)
+  }
+
+
+  template_structure <- file.path(create_project_path, "src/template_structure")
+
+  if (!file.exists(template_structure)) {
+    stop(paste("The file does not exist:", template_structure))
+  }
+
+  dirs <- readLines(template_structure, warn = FALSE)
+  # Remove any empty or whitespace-only lines (e.g., the incomplete final line)
+  dirs <- dirs[dirs != ""]
+  dirs <- trimws(dirs)
+  for (dir in dirs) {
+    dir_to_create <- file.path(project_path, dir)
+    if (!dir.exists(dir_to_create)) {
+      dir.create(dir_to_create, recursive = TRUE)
+    }
+  }
+
+  file.copy(file.path(create_project_path, "src", "template_README_main.md"), file.path(project_path, "README.md"),overwrite = FALSE)
+  file.copy(file.path(create_project_path, "src", "template_README_data.md"), file.path(project_path, "data/README.md"),overwrite = FALSE)
+  file.copy(file.path(create_project_path, "src", "template_README_manuscript.md"), file.path(project_path, "manuscript/README.md"),overwrite = FALSE)
+  file.copy(file.path(create_project_path, "src", "template_README_src.md"), file.path(project_path, "src/README.md"),overwrite = FALSE)
+  print("Project template generated.")
 }
