b998072b87a6fd2fbef1ae09ea6e37121f1c20df
Ali <farnudia@iarc.who.int>
2025-03-07 11:03:52 +0100

    simplify readme creation pieline

--- create_project.sh
+++ create_project.sh
@@ -12,52 +12,34 @@ check_for_help "$@"
 # Parse the arguments
 parse_args "$@"
 
-projec_dir="$location/$project_name"
+project_dir="$location/$project_name"
 
 if [ ! -d "$location" ]; then
     echo "Could not find '$location'. Please check the location and try again."
     exit 2
 fi
 
-if [ -d "$projec_dir" ]; then
-    echo "$projec_dir already exists. Please select a different project name, or  a different location."
+if [ -d "$project_dir" ]; then
+    echo "$project_dir already exists. Please select a different project name, or  a different location."
     exit 3
 fi
 
-mkdir "$projec_dir"
+mkdir "$project_dir"
 
 structure_file="${DIR_source}/template_structure"
 while IFS= read -r line || [ -n "$line" ]; do
-    full_path="$projec_dir$line"
+    full_path="$project_dir$line"
     
     if [[ $line == */ ]]; then
         mkdir -p "$full_path"
-    else
-        # For README.md files, copy the specific template based on the file path
-        case "$line" in
-            "/data/README.md")
-                cp "${DIR_source}/template_README_data.md" "$full_path"
-                ;;
-            "/src/README.md")
-                cp "${DIR_source}/template_README_src.md" "$full_path"
-                ;;
-            "/analysis/README.md")
-                cp "${DIR_source}/template_README_analysis.md" "$full_path"
-                ;;
-            "/manuscript/README.md")
-                cp "${DIR_source}/template_README_manuscript.md" "$full_path"
-                ;;
-            "/README.md")
-                cp "${DIR_source}/template_README_main.md" "$full_path"
-                ;;
-            *)
-                # For other files, create empty files for paths
-                mkdir -p "$(dirname "$full_path")" # Ensure parent directories exist
-                touch "$full_path"
-                ;;
-        esac
     fi
 done < "$structure_file"
 
+cp "${DIR_source}/template_README_main.md" "${project_dir}/README.md";
+cp "${DIR_source}/template_README_analysis.md" "${project_dir}/analysis/README.md";
+cp "${DIR_source}/template_README_data.md" "${project_dir}/data/README.md";
+cp "${DIR_source}/template_README_manuscript.md" "${project_dir}/manuscript/README.md";
+cp "${DIR_source}/template_README_src.md" "${project_dir}/src/README.md";
+
 # gitignore
-cp "${DIR_source}/template_gitignore" "$projec_dir/.gitignore"
+cp "${DIR_source}/template_gitignore" "$project_dir/.gitignore"
